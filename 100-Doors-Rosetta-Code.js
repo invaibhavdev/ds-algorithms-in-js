0ded070c6d3eb7a90e29ea7222e99b13741c50a7
Vaibhav Misra <invaibhavdev@gmail.com>
2020-06-28 09:21:57 +0530

    Update 100-Doors-Rosetta-Code.js

--- 100-Doors-Rosetta-Code.js
+++ 100-Doors-Rosetta-Code.js
@@ -16,14 +16,20 @@ Return the final result in an array, with only the door number included in the a
 
 function getFinalOpenedDoors(numDoors) {
   let doors = [];
+  
+  // function to set generate given number of doors and set their initial state to false
   const generateDoors = (numberOfDoors) => {
     for (let i = 1; i <= numberOfDoors; i++) {
       doors[i] = false;
     }
   }
+  
+  // function to toggle the current state of any door given the door number
   const toggleDoorState = (doorNumber) => {
     doors[doorNumber] = !doors[doorNumber];
   }
+  
+  // function to make passes equal to the number of doors and toggle the state
   const makePasses = (passes) => {
     for (let i = 1; i<= passes; i++) {
       for (let j = 1; j <= passes; j++) {
@@ -33,17 +39,18 @@ function getFinalOpenedDoors(numDoors) {
       }
     }
   }
+  
+  // function to get the number of doors that are open after the final pass
   const sendFinalStatus = (doors) => {
     const finalList = [];
-    // const finalIndicesList = doors.map((val, index) => val );
     for (let i = 1; i <= doors.length; i++) {
       if (doors[i]) {
         finalList.push(i);
       }
     }
-    // console.log(finalList);
     return finalList;
   }
+  
   generateDoors(numDoors);
   makePasses(numDoors);
   return sendFinalStatus(doors);
